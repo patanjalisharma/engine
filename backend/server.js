3acd41f2fb755b210fb29fa6212de05876a5106a
patanjalisharma <patanjali.sharma03@gmail.com>
2026-04-02 17:00:58 +0530

    Fix Cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://engine-five.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use("/api/document", documentRoutes)
 app.use("/api/search", searchRoutes)
 app.use("/api/crawl", crawlRoutes)
